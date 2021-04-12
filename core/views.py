658dbd8a1c989e5b75d011680fafc3d986f780c1
System Administrator <root@Pavels-MacBook-Pro.local>
2021-04-13 00:49:56 +0300

    Added some custom forms for the admin area

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def home(request):
     return render(request, "index.html")
+
+class SignupView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = '/accounts/login'
+    template_name = 'registration/signup.html'
